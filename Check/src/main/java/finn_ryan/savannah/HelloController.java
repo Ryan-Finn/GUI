d4739679d9b48e04cef715bcd2c6b1446f2c6e65
Finn, Ryan <ryan.finn@mines.sdsmt.edu>
2022-01-27 02:47:41 -0700

    JavaFX Check init

---
+++ Check/src/main/java/finn_ryan/savannah/HelloController.java
@@ -0,0 +1,14 @@
+package finn_ryan.savannah;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
